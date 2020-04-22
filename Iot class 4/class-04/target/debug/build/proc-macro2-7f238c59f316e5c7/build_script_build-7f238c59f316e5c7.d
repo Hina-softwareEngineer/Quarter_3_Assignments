91d55185f342eb814a666e30e00961a95782d004
HINA KHADIM <hinakhadim2002@gmail.com>
2020-04-22 18:54:05 +0500

    .

--- Iot class 4/class-04/target/debug/build/proc-macro2-7f238c59f316e5c7/build_script_build-7f238c59f316e5c7.d
+++
@@ -1,5 +0,0 @@
-/home/hina/Documents/Python/Quarter_3_Assignments/Iot class 4/class-04/target/debug/build/proc-macro2-7f238c59f316e5c7/build_script_build-7f238c59f316e5c7: /home/hina/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.10/build.rs
-
-/home/hina/Documents/Python/Quarter_3_Assignments/Iot class 4/class-04/target/debug/build/proc-macro2-7f238c59f316e5c7/build_script_build-7f238c59f316e5c7.d: /home/hina/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.10/build.rs
-
-/home/hina/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.10/build.rs:
--- Iot class 4/class-04/target/debug/deps/futures-45924430d0594079.d
+++
@@ -1,7 +0,0 @@
-/home/hina/Documents/Python/Quarter_3_Assignments/Iot class 4/class-04/target/debug/deps/futures-45924430d0594079.rmeta: /home/hina/.cargo/registry/src/github.com-1ecc6299db9ec823/futures-0.3.4/src/lib.rs
-
-/home/hina/Documents/Python/Quarter_3_Assignments/Iot class 4/class-04/target/debug/deps/libfutures-45924430d0594079.rlib: /home/hina/.cargo/registry/src/github.com-1ecc6299db9ec823/futures-0.3.4/src/lib.rs
-
-/home/hina/Documents/Python/Quarter_3_Assignments/Iot class 4/class-04/target/debug/deps/futures-45924430d0594079.d: /home/hina/.cargo/registry/src/github.com-1ecc6299db9ec823/futures-0.3.4/src/lib.rs
-
-/home/hina/.cargo/registry/src/github.com-1ecc6299db9ec823/futures-0.3.4/src/lib.rs:
