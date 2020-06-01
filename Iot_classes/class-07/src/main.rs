a5d4bccc63dbd427a3b5c06505d5986f57e7ac09
HINA KHADIM <hinakhadim2002@gmail.com>
2020-06-01 16:21:53 +0500

    :smiley: :smilye: finally functionality complete!

--- Iot_classes/class-07/src/main.rs
+++ Iot_classes/class-07/src/main.rs
@@ -59,72 +59,105 @@ use rustc_serialize::json::Json;
 
 use std::io::Read;
 
+
+
+// #[get("/num?<number>")]
+// fn hello(number :u8) -> String {
+//     format!("The number is {}!", number+2)
+// }
+
+use rocket::request::Form;
+use rocket::response::content::Html;
+
+#[derive(FromForm)]
+struct Number{
+    number : i8,
+}
+
+
+#[post("/num", data = "<number>")]
+fn hello(number : Form<Number>) -> Html<String> {
+   let h1=format!("<h1 style='color: blue;'>hello guys {}</h1>", number.number + 2);
+    Html(h1)
+}
+
+
+fn main() {
+    rocket::ignite().mount("/hello", routes![hello]).launch();
+}
+
 // openweathermapapi.com
 
 // Web_Attributes or Web_Methods Get, Post, Put, Delete
 // get request using rocket
 
-#[get("/")]
-// [rocket::get("/")]
-fn hello() -> String {
-    let path = Path::new("api.json");
-    let display = path.display();
-    println!("{:?} {:?}", path, display);
-
-    let mut file = match File::create(path) {
-        Ok(file) => file,
-        Err(_) => panic!("File could not create."),
-    };
-
-    match reqwest::get("http://jsonplaceholder.typicode.com/users") {
-        Ok(mut response) => match response.text() {
-            Ok(text) => match file.write_all(text.as_bytes()) {
-                Ok(_) => println!("Data write in file!"),
-                Err(_) => println!("The error is "),
-            },
-            Err(_) => println!("The response is not come from the server."),
-        },
-        Err(_) => println!("Server could not established the connecton."),
-    }
-
-    let mut file = match File::open(&path) {
-        Ok(file) => file,
-        Err(_) => panic!("{} file open Error: e.description()"),
-    };
-
-    let mut buffer = String::new();
-    file.read_to_string(&mut buffer).unwrap(); // unmanagable errors and we can't write text in it. (unwrap)
-
-    let json = Json::from_str(&buffer).unwrap();
-    let result = format!("The apis is {}", json[0].find_path(&["name"]).unwrap());
-
-    // let result = format!(
-    //     "The temperature of Karachi is {}",
-    //     json.find_path(&["name"]).unwrap()
-    // );
-
-    result
-}
+// #[get("/<number>")]
+// fn add(number : i8)->String{
+//     format!("The number you give is : {}", number+2)
+// }
 
 
-use rocket::request::Form;
+// #[get("/home")]
+// // [rocket::get("/")]
+// fn hello() -> String {
+//     let path = Path::new("api.json");
+//     let display = path.display();
+//     println!("{:?} {:?}", path, display);
+
+//     let mut file = match File::create(path) {
+//         Ok(file) => file,
+//         Err(_) => panic!("File could not create."),
+//     };
+
+//     match reqwest::get("http://jsonplaceholder.typicode.com/users") {
+//         Ok(mut response) => match response.text() {
+//             Ok(text) => match file.write_all(text.as_bytes()) {
+//                 Ok(_) => println!("Data write in file!"),
+//                 Err(_) => println!("The error is "),
+//             },
+//             Err(_) => println!("The response is not come from the server."),
+//         },
+//         Err(_) => println!("Server could not established the connecton."),
+//     }
+
+//     let mut file = match File::open(&path) {
+//         Ok(file) => file,
+//         Err(_) => panic!("{} file open Error: e.description()"),
+//     };
+
+//     let mut buffer = String::new();
+//     file.read_to_string(&mut buffer).unwrap(); // unmanagable errors and we can't write text in it. (unwrap)
+
+//     let json = Json::from_str(&buffer).unwrap();
+//     let result = format!("The apis is {}", json[0].find_path(&["name"]).unwrap());
+
+//     // let result = format!(
+//     //     "The temperature of Karachi is {}",
+//     //     json.find_path(&["name"]).unwrap()
+//     // );
+
+//     result
+// }
+
+
+// use rocket::request::Form;
 
 
 
-#[derive(FromForm)]
-pub struct Todo {
-    description: String
-}
+// #[derive(FromForm)]
+// pub struct Todo {
+//     description: String
+// }
 
-#[post("/", data = "<todo_form>")]
-fn new(todo_form: Form<Todo>) -> Flash<Redirect> {
-    let todo = todo_form.into_inner();
-    if todo.description.is_empty() {
-        Flash::error(Redirect::to("/"), "Description cannot be empty.")
-    } else  {
-        Flash::success(Redirect::to("/"), "Todo successfully added.")
-    } 
-}
+// #[post("/", data = "<todo_form>")]
+// fn new(todo_form: Form<Todo>) -> Flash<Redirect> {
+//     let todo = todo_form.into_inner();
+//     if todo.description.is_empty() {
+//         Flash::error(Redirect::to("/"), "Description cannot be empty.")
+//     } else  {
+//         Flash::success(Redirect::to("/"), "Todo successfully added.")
+//     } 
+// }
 
 
 // use rocket::Data;
@@ -140,6 +173,6 @@ fn new(todo_form: Form<Todo>) -> Flash<Redirect> {
 //     task.stream_to_file("upload.txt").map(|n| n.to_string())
 // }
 
-fn main() {
-    rocket::ignite().mount("/", rocket::routes![hello]).launch();
-}
+// fn main() {
+//     rocket::ignite().mount("/add", rocket::routes![add]).launch();
+// }
