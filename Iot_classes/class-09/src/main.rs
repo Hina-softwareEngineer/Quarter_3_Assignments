ef6676e268dd12ac92b388078fe0801a9df22bee
HINA KHADIM <hinakhadim2002@gmail.com>
2020-06-27 13:17:42 +0500

    connection successful

--- Iot_classes/class-09/src/main.rs
+++ Iot_classes/class-09/src/main.rs
@@ -1,82 +1,8 @@
-
 use std::io;
 
 use mysql::prelude::*;
 use mysql::*;
 
-
-#[derive(Debug, PartialEq, Eq)]
-struct Payment {
-    customer_id: i32,
-    amount: i32,
-    account_name: Option<String>,
-}
-fn insert(){
-// let url = "mysql://root:root@localhost:3306/Rust_testing";
-
-// let pool = Pool::new(url);
-
-let pool = Pool::new("mysql://root:root@localhost:3306/Rust_testing").unwrap();
-
-    println!("Pool : {:?}", pool);
-
-    // // Let's create payment table.
-    // // Unwrap just to make sure no error happened.
-    // let mut conn=pool.get_conn();
-    // println!("conection {:?}", conn);
-
-
-let mut conn = pool.get_conn();
-
-// Let's create a table for payments.
-conn.query_drop(
-    r"CREATE TEMPORARY TABLE payment (
-        customer_id int not null,
-        amount int not null,
-        account_name text
-    )");
-
-let payments = vec![
-    Payment { customer_id: 1, amount: 2, account_name: None },
-    Payment { customer_id: 3, amount: 4, account_name: Some("foo".into()) },
-    Payment { customer_id: 5, amount: 6, account_name: None },
-    Payment { customer_id: 7, amount: 8, account_name: None },
-    Payment { customer_id: 9, amount: 10, account_name: Some("bar".into()) },
-];
-
-// Now let's insert payments to the database
-conn.exec_batch(
-    r"INSERT INTO payment (customer_id, amount, account_name)
-      VALUES (:customer_id, :amount, :account_name)",
-    payments.iter().map(|p| params! {
-        "customer_id" => p.customer_id,
-        "amount" => p.amount,
-        "account_name" => &p.account_name,
-    })
-);
-
-// Let's select payments from database. Type inference should do the trick here.
-let selected_payments = conn
-    .query_map(
-        "SELECT customer_id, amount, account_name from payment",
-        |(customer_id, amount, account_name)| {
-            Payment { customer_id, amount, account_name }
-        },
-    );
-
-// Let's make sure, that `payments` equals to `selected_payments`.
-// Mysql gives no guaranties on order of returned rows
-// without `ORDER BY`, so assume we are lucky.
-assert_eq!(payments, selected_payments);
-println!("Yay!");
-
-}
-
-fn main(){
-    insert();
-}
-
-/*
 #[derive(Debug, PartialEq, Eq)]
 struct Student {
     sid: i32,
@@ -86,10 +12,6 @@ struct Student {
 }
 
 fn main() {
-    // let pool = Pool::new("mysql://root:root@localhost:3306/Rust_testing");
-
-    // println!("Pool : {:?}", pool);
-
     println!("Enter your sid");
     let mut sid = String::new();
     io::stdin().read_line(&mut sid);
@@ -116,8 +38,10 @@ fn main() {
         email: Some(email),
         age: Some(age),
     };
-    insert(student);
+    // insert(student);
     // fetch();
+    // update(student);
+    delete(student);
 }
 
 fn insert(student: Student) {
@@ -127,24 +51,10 @@ fn insert(student: Student) {
 
     // // Let's create payment table.
     // // Unwrap just to make sure no error happened.
-    let mut conn=pool.get_conn();
+    let mut conn = pool.get_conn().unwrap();
     println!("conection {:?}", conn);
-    let pool=pool.clone();
-    let students = vec![
-        student
-    ];
+    let students = vec![student];
 
-
-    let mut result=pool.prep_exec("INSERT INTO student (sid) VALUES (:sid)");
-    // conn.query_drop(
-    //     r"CREATE TEMPORARY TABLE student1 (
-    //         sid int not null,
-    //         name text,
-    //         email text,
-    //         age text
-    //     )")?;
-
-    println!("rsutl : {:?}", result);
     // Let's insert payments to the database
     // We will use into_iter() because we do not need to map Stmt to anything else.
     // Also we assume that no error happened in `prepare`.
@@ -164,111 +74,162 @@ fn insert(student: Student) {
     //     }
     // }
 
-
-    // let mut stmt1=pool.prepare("")
-
-    // conn.exec_batch(
-    //     r"INSERT INTO student1 (sid, name, email, age)
-    //       VALUES (:sid, :name, :email)",
-    //     students.iter().map(|p| params! {
-    //         "sid" => p.sid,
-    //         "name" => p.name,
-    //         "email" => &p.email,
-    //         "age"=>&p.age
-    //     })
-    // );
+    conn.exec_batch(
+        r"INSERT INTO student (sid, name, email, age)
+          VALUES (:sid, :name, :email, :age)",
+        students.iter().map(|p| {
+            params! {
+                "sid" => p.sid,
+                "name" => &p.name,
+                "email" => &p.email,
+                "age"=>&p.age
+            }
+        }),
+    )
+    .unwrap();
     println!("Added succesfully");
 }
 
-/* update(student:Student){
-    let pool = my::Pool::new("mysql://root:@localhost:3306/test").unwrap();
+fn update(student:Student){
+    let pool = Pool::new("mysql://root:root@localhost:3306/Rust_testing").unwrap();
+    let mut conn = pool.get_conn().unwrap();
     // // Let's create payment table.
     // // Unwrap just to make sure no error happened.
     let students = vec![
        student
     ];
 
+
+    conn.exec_batch(
+        r"UPDATE student 
+        set
+        name=:name,
+        email=:email,
+        age=:age 
+        where sid=:sid",
+        students.iter().map(|p| {
+            params! {
+                "sid" => p.sid,
+                "name" => &p.name,
+                "email" => &p.email,
+                "age"=>&p.age
+            }
+        }),
+    ).unwrap();
+
+
+    println!("updated successfully");
+
     // Let's insert payments to the database
     // We will use into_iter() because we do not need to map Stmt to anything else.
     // Also we assume that no error happened in `prepare`.
-    for mut stmt in pool.prepare(r"Update tblstudent
-                                    set name=:name,
-                                    email=:email, age=:age
-                                    where sid=:sid
-                                    ").into_iter() {
-        for s in students.iter() {
-            // `execute` takes ownership of `params` so we pass account name by reference.
-            // Unwrap each result just to make sure no errors happened.
-            stmt.execute(params!{
-                "sid" => s.sid,
-                "name" => &s.name,
-                "email" => &s.email,
-                "age" => &s.age,
-            }).unwrap();
-        }
-    }
+    // for mut stmt in pool.prepare(r"Update tblstudent
+    //                                 set name=:name,
+    //                                 email=:email, age=:age
+    //                                 where sid=:sid
+    //                                 ").into_iter() {
+    //     for s in students.iter() {
+    //         // `execute` takes ownership of `params` so we pass account name by reference.
+    //         // Unwrap each result just to make sure no errors happened.
+    //         stmt.execute(params!{
+    //             "sid" => s.sid,
+    //             "name" => &s.name,
+    //             "email" => &s.email,
+    //             "age" => &s.age,
+    //         }).unwrap();
+    //     }
+    // }
 }
 
 
 
 
 fn delete(student: Student){
-    let pool = my::Pool::new("mysql://root:@localhost:3306/test").unwrap();
+    let pool = Pool::new("mysql://root:root@localhost:3306/Rust_testing").unwrap();
+
+    println!("Pool : {:?}", pool);
+
+    // // Let's create payment table.
+    // // Unwrap just to make sure no error happened.
+    let mut conn = pool.get_conn().unwrap();
     // // Let's create payment table.
     // // Unwrap just to make sure no error happened.
     let students = vec![
         student
     ];
 
+
+    conn.exec_batch(
+        r"delete from student 
+        where sid=:sid",
+        students.iter().map(|p| {
+            params! {
+                "sid" => p.sid,
+                // "name" => &p.name,
+                // "email" => &p.email,
+                // "age"=>&p.age
+            }
+        }),
+    ).unwrap();
     // Let's insert payments to the database
     // We will use into_iter() because we do not need to map Stmt to anything else.
     // Also we assume that no error happened in `prepare`.
-    for mut stmt in pool.prepare(r"delete from tblstudent
-                                    where sid=:sid
-                                    ").into_iter() {
-        for s in students.iter() {
-            // `execute` takes ownership of `params` so we pass account name by reference.
-            // Unwrap each result just to make sure no errors happened.
-            stmt.execute(params!{
-                "sid" => s.sid,
-                "name" => &s.name,
-                "email" => &s.email,
-                "age" => &s.age,
-            }).unwrap();
-        }
-    }
+    // for mut stmt in pool.prepare(r"delete from tblstudent
+    //                                 where sid=:sid
+    //                                 ").into_iter() {
+    //     for s in students.iter() {
+    //         // `execute` takes ownership of `params` so we pass account name by reference.
+    //         // Unwrap each result just to make sure no errors happened.
+    //         stmt.execute(params!{
+    //             "sid" => s.sid,
+    //             "name" => &s.name,
+    //             "email" => &s.email,
+    //             "age" => &s.age,
+    //         }).unwrap();
+    //     }
+    // }
 }
 
 
 
-
-
-
-fn fetch(){
-    let pool = my::Pool::new("mysql://root:@localhost:3306/test").unwrap();
+fn fetch() {
+    let pool = Pool::new("mysql://root:root@localhost:3306/Rust_testing").unwrap();
     // Let's select payments from database
-    let selected_students: Vec<Student> =
-    pool.prep_exec("SELECT sid, name, email, age from tblstudent", ())
-    .map(|result| { // In this closure we will map `QueryResult` to `Vec<Payment>`
-        // `QueryResult` is iterator over `MyResult<row, err>` so first call to `map`
-        // will map each `MyResult` to contained `row` (no proper error handling)
-        // and second call to `map` will map each `row` to `Payment`
-        result.map(|x| x.unwrap()).map(|row| {
-            // ⚠️ Note that from_row will panic if you don't follow your schema
-            let (sid, name, email, age) = my::from_row(row);
-            Student {
-                sid: sid,
-                name: name,
-                email: email,
-                age: age,
-            }
-        }).collect() // Collect payments so now `QueryResult` is mapped to `Vec<Payment>`
-    }).unwrap(); // Unwrap `Vec<Payment>`
 
-    for s in 0..selected_students.len(){
-        println!("ID: {} Name: {:?} Email: {:?} Age: {:?}",selected_students[s].sid,selected_students[s].name,
-                        selected_students[s].email,selected_students[s].age);
+    let mut conn = pool.get_conn().unwrap();
+    let selected_payments = conn
+        .query_map(
+            "SELECT sid, name, email, age from student",
+            |(sid, name, email, age)| Student {
+                sid,
+                name,
+                email,
+                age,
+            },
+        )
+        .unwrap();
 
-    }
-}*/
-*/
+    println!("rsult : {:?}", selected_payments);
+
+    // let selected_students: Vec<Student> =
+    // pool.prep_exec("SELECT sid, name, email, age from tblstudent", ())
+    // .map(|result| { // In this closure we will map `QueryResult` to `Vec<Payment>`
+    //     // `QueryResult` is iterator over `MyResult<row, err>` so first call to `map`
+    //     // will map each `MyResult` to contained `row` (no proper error handling)
+    //     // and second call to `map` will map each `row` to `Payment`
+    //     result.map(|x| x.unwrap()).map(|row| {
+    //         // ⚠️ Note that from_row will panic if you don't follow your schema
+    //         let (sid, name, email, age) = my::from_row(row);
+    //         Student {
+    //             sid: sid,
+    //             name: name,
+    //             email: email,
+    //             age: age,
+    //         }
+    //     }).collect() // Collect payments so now `QueryResult` is mapped to `Vec<Payment>`
+    // }).unwrap(); // Unwrap `Vec<Payment>`
+
+    // for s in 0..selected_students.len(){
+    //     println!("ID: {} Name: {:?} Email: {:?} Age: {:?}",selected_students[s].sid,selected_students[s].name,
+    //                     selected_students[s].email,selected_students[s].age);
+}
